2d6b13f31c780728a3c2d0a096a2176f9247d86f
supgeek-rod <supgeek-rod@outlook.com>
2023-06-08 18:39:01 +0800

    tables

---
+++ app/Tenements/ePartV2/Databases/Migrations/2023_06_08_102305_create_category_attributes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('epart_v2_category_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id')->index()->comment('Category ID');
+
+            $table->string('name')->index()->comment('Attr name');
+            $table->json('values')->comment('Attr values');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('epart_v2_category_attributes');
+    }
+};
