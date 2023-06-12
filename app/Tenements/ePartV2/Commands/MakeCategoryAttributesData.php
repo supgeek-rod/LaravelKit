fac0f302cc71bc9f2b9e5957583ae9b24d21fce9
supgeek-rod <supgeek-rod@outlook.com>
2023-06-12 17:51:17 +0800

    PartToElasticSearch

--- app/Tenements/ePartV2/Commands/MakeCategoryAttributesData.php
+++ app/Tenements/ePartV2/Commands/MakeCategoryAttributesData.php
@@ -7,6 +7,7 @@ use App\Tenements\ePartV2\Models\CategoryAttribute;
 use App\Tenements\ePartV2\Models\Part;
 use App\Tenements\ePartV2\Models\PartAttribute;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
 class MakeCategoryAttributesData extends Command
@@ -17,7 +18,7 @@ class MakeCategoryAttributesData extends Command
      * @var string
      */
     protected $signature = 'ePartV2:make-category-attributes-data
-                           {category=all : 分类 ID，默认为全部}';
+                           {category : 分类 ID，`all` 为全部分类并分发队列}';
 
     /**
      * The console command description.
@@ -42,32 +43,52 @@ class MakeCategoryAttributesData extends Command
      */
     public function handle()
     {
-        $this->line('## Start ' . __METHOD__);
-
         $categoryId = $this->argument('category');
         $categoryQuery = Category::query()->select('id', 'name')->whereHas('parts')->withCount('parts')->orderBy('id');
-        if ($categoryId !== 'all') {
-            $categoryQuery->where('id', $categoryId);
+
+        // 如果 category = all, 则进行队列分发
+        if ($categoryId === 'all') {
+            foreach ($categoryQuery->get() as $category) {
+                // Artisan::queue("ePartV2:make-category-attributes-data {$category->id}");
+                $this->executeHandler($category);
+            }
         }
 
-        foreach (($categories = $categoryQuery->get()) as $index => $category) {
-            $this->newLine()->line('#' . $index + 1 . '/' . $categories->count() . " [{$category->name}]({$category->id}): Start");
+        // 如果指定了 category, 则直接执行处理方法
+        else {
+            $this->executeHandler($categoryQuery->where('id', $categoryId)->first());
+        }
+
+        $this->newLine()->line(__METHOD__ . ': done!');
+    }
+
+    /**
+     * 处理方法
+     */
+    protected function executeHandler($category)
+    {
+        $this->newLine()->line('## Start ' . __METHOD__);
+
+        if ($category) {
+            $this->line("#{$category->id} ({$category->name}): Start");
             $this->line('Has ' . $category->parts_count . ' parts');
 
             $startTime = microtime(true);
 
             try {
                 $categoryAttributes = $this->getCategoryAttributes($category);
-                // $this->saveCategoryAttributes($category, $categoryAttributes);
+                $this->saveCategoryAttributes($category, $categoryAttributes);
 
-                $this->line('Duration: ' . round((microtime(true) - $startTime), 3) . 's');
                 $this->line('Has ' . count($categoryAttributes) . ' distinct attributes');
+                $this->line('Duration: ' . round((microtime(true) - $startTime), 3) . 's');
             } catch (\Exception $exception) {
                 $this->error('Has error');
                 $this->error($exception->getMessage());
             }
 
             $this->line('Usage memory ' . round(memory_get_usage() / 1024 / 1024) . 'MB');
+        } else {
+            $this->error('Category is not exists');
         }
     }
 
